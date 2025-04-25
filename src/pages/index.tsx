c70f268f1a0a0a3616bb71dba1b55ac6e2cfd458
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2025-04-25 14:58:49 +0530

    trash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,12 @@
 import React from "react";
-import Header from "../../components/Header";
-import Separator from "../../components/Separator";
-import AboutMe from "../../components/AboutMe";
-import Footer from "../../components/Footer";
-import Info from "../../components/Info";
-import Blogs from "../../components/Blogs";
-import Highlights from "../../components/Highlights";
-import Projects from "../../components/Projects";
-import Head from "next/head";
+import NavBar from "../components/ NavBar";
 
-function Index() {
+const Index = () => {
   return (
-    <div className="karla">
-      <Head>
-        <title>Ajo Alex</title>
-      </Head>
-      <div className="container md:w-1/2 mx-auto py-20 px-4">
-        <Header />
-        <Separator label="Highlights" />
-        <Highlights />
-        <Separator label="About Me" />
-        <AboutMe />
-        <Separator label="Info" />
-        <Info />
-        <Separator label="Latest Blogs" />
-        <Blogs />
-        <Separator label="Projects" />
-        <Projects />
-        <Footer />
-      </div>
+    <div className="w-2/3 mx-auto">
+      <NavBar />
     </div>
   );
-}
+};
 
 export default Index;
